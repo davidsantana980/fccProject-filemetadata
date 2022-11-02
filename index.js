4f953d1c2feb4142358b3ecda71c92243df5cfcd
davidsantana980 <108551512+davidsantana980@users.noreply.github.com>
2022-11-02 14:18:09 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
+const multer = require("multer");
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, './fileUploads')
+  },
+  filename: (req, file, callback) => {
+    callback(null, `${file.originalname}-${Date.now()}`)
+  }
+})
+ 
+const upload = multer({ storage: fileStorageEngine })
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  let file = req.file;
+  res.json({
+    name: file.originalname, 
+    type: file.mimetype, 
+    size: file.size
+  })
+})
 
 
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
+const port = process.env.PORT;
+app.listen(port, () => {
   console.log('Your app is listening on port ' + port)
 });
